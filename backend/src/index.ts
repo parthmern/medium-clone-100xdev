65535a309ffed0df5e553ad83d6c18c40edec0cf
parthmern <parthmern@gmail.com>
2024-07-13 00:05:41 -0400

    index added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,35 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
 
-const app = new Hono()
+// Create the main Hono app
+const app = new Hono();
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  console.log(c);
+  // c.json()   -- for json reponse
+	return c.text('hello from hono js with cloudflare workers/wrangler')
 })
 
-export default app
+app.post('/api/v1/signup', (c) => {
+	return c.text('signup route')
+})
+
+app.post('/api/v1/signin', (c) => {
+	return c.text('signin route')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+	const id = c.req.param('id')
+	console.log(id);
+	return c.text('get blog route')
+})
+
+app.post('/api/v1/blog', (c) => {
+
+	return c.text('signin route')
+})
+
+app.put('/api/v1/blog', (c) => {
+	return c.text('signin route')
+})
+
+export default app;
