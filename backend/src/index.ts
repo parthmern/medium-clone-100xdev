24a85cf0765fbd8c5647ab84ef98fd4db71dbe93
parthmern <parthmern@gmail.com>
2024-07-16 22:57:47 -0400

    Signup route addeD

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono';
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-
+import { decode, sign, verify } from 'hono/jwt'
 
 // Create the main Hono app
 // const app = new Hono<{
@@ -13,27 +13,85 @@ import { withAccelerate } from '@prisma/extension-accelerate'
 const app = new Hono();
 
 app.get('/', (c) => {
-  console.log(c);
+  // console.log(c);
   // c.json()   -- for json reponse
+
+  // @ts-ignore
+  const DATABASE_URL = c.env.DATABASE_URL ;
+
+	console.log(DATABASE_URL);
+
 	return c.text('hello from hono js with cloudflare workers/wrangler')
 })
 
-app.post('/api/v1/signup', (c) => {
+app.post('/api/v1/signup', async (c) => {
 
 	// in hono how to get ENV vars
 	// @ts-ignore
 	const DATABASE_URL = c.env.DATABASE_URL ;
+
+	console.log(DATABASE_URL);
 
 	// this is IMP acceleatate needed for connection pooling in serverless backend
 	const prisma = new PrismaClient({
 		datasourceUrl: DATABASE_URL,
 	}).$extends(withAccelerate())
 
-	return c.text('signup route')
+
+	const body = await c.req.json();
+
+	const createdUser = await prisma.user.create(
+		{
+			data : {
+				email : body.email ,
+				password : body.password ,
+			}
+		}
+	)
+
+	// jwt library cannot work in cloudflare workers so hono has its own
+	
+	const jwtToken = await sign(
+		{
+			id : createdUser.id
+		},
+		"secret"
+	);
+	  
+
+
+	return c.json(
+		{
+			success : true,
+			message : "signup route user created",
+			createdUser ,
+			jwt : jwtToken
+		}
+	)
 })
 
-app.post('/api/v1/signin', (c) => {
-	return c.text('signin route')
+app.post('/api/v1/signin', async (c) => {
+	// @ts-ignore
+	const DATABASE_URL = c.env.DATABASE_URL ;
+
+	const prisma = new PrismaClient({
+		datasourceUrl: DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+	const user = await prisma.user.findUnique({
+		where: {
+			email: body.email
+		}
+	});
+
+	if (!user) {
+		c.status(403);
+		return c.json({ error: "user not found" });
+	}
+
+	const jwt = await sign({ id: user.id }, 'secret');
+	return c.json({ jwt });
 })
 
 app.get('/api/v1/blog/:id', (c) => {
